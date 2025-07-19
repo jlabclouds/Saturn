942cea65b22fe6a81c6002d6e8edd3be7c98fab8
jlab <jlabcloud@gmail.com>
2025-07-19 17:26:23 -0400

    add nav pages and js

--- Saturn.Web/Todo.cs
+++ Saturn.Web/Todo.cs
@@ -1,7 +1,22 @@
 ﻿namespace Saturn.Web;
 
-public class TodoItem(HttpClient httpClient)
+public class TodoItemClient(HttpClient httpClient)
 {
-    public string? Title { get; set; }
-    public bool IsDone { get; set; }
+    public async Task<TodoItemClient[]> GetTodosAsync(int maxItems = 10, CancellationToken cancellationToken = default)
+    {
+        List<TodoItemClient>? todos = null;
+        await foreach (var todo in httpClient.GetFromJsonAsAsyncEnumerable<TodoItemClient>("/todo", cancellationToken))
+        {
+            if (todos?.Count >= maxItems)
+            {
+                break;
+            }
+            if (todo is not null)
+            {
+                todos ??= [];
+                todos.Add(todo);
+            }
+        }
+        return todos?.ToArray() ?? [];
+    }
 }
