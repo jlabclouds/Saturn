b5bcd4dea406a6c5d74edde0d20be5d05254a947
jlabclouds <jlabcloud@outlook.com>
2025-07-20 14:30:07 -0400

    update

--- Saturn.Database/CosmoDB.cs
+++
@@ -1,8 +0,0 @@
-var cosmos = builder.AddCosmosDb("cosmosdb, password")
-                    .WithLifetime(ContainerLifetime.Persistent)
-                    .WithDataVolume()
-                    .WithDataBindMount(source: @"C:\CosmosDB\Data");     
-var cosmosdb = cosmos.AddDatabase("cosmosdb");
-builder.AddProject<Projects.Aspire_#>()
-       .WithReference(cosmosdb)
-       .WaitFor(cosmosdb);
--- Saturn.Database/OciDB.cs
+++
@@ -1,8 +0,0 @@
-var oracle = builder.AddOracle("oracle, password")
-                    .WithLifetime(ContainerLifetime.Persistent);
-                    .WithDataVolume() 
-                    .WithDataBindMount(source: @"C:\Oracle\Data"); 
-var oracledb = oracle.AddDatabase("oracledb");
-builder.AddProject<Projects.Aspire_#>()
-       .WithReference(oracledb);
-       .WaitFor(oracledb);
